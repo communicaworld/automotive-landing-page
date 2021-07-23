ca861083a796e24375dfe9b898e88c1265e4eb95
danpaquette <dm.paquette@gmail.com>
2021-07-22 20:31:42 -0400

    Landing page code, minus logos and video.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# automotive-landing-page
+# Automotive Landing Page
+
+## Preparing the Environment
+
+- **Install Node.js**: [Follow the instructions here](https://nodejs.org/en/download/) to install Node.js on your particular operating system.
+
+- **Install Gulp**: Run `npm install -g gulp-cli`, or [follow the instructions](https://gulpjs.com/docs/en/getting-started/quick-start/) here to install the Gulp CLI
+
+## Compile Resources
+
+- **Run Gulp**: In a new terminal window, run `gulp` to compile project resources.
+
+- **(Optional) Gulp Watch**: You can use `gulp watch` to watch for resource changes and recompile them as you work.
+
+## Code Style
+
+#### HTML
+
+[Code Guide](http://codeguide.co/#html)
+
+- Self closing tags must use a trailing forward slash (e.g. `<img src="#" />`.
+- Tags that do not require a closing tag (e.g. `<li>`) should always be closed with their corresponding closing tag (e.g. `<li></li>`)
+- Use https for all hosted third party inclusions. We don't use protocol-relative URLs for practical and security reasons. [See Paul Irish's Update](http://www.paulirish.com/2010/the-protocol-relative-url/)
+- Use [WAI-ARIA](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA) where necessary.
+
+#### CSS
+
+[Code Guide](http://codeguide.co/#css)
+
+- When feasible, default color palettes should comply with [WCAG color contrast guidelines](http://www.w3.org/TR/WCAG20/#visual-audio-contrast).
+- Always include alternative styles for `:focus` styles (via e.g. `outline: none;`). See [this A11Y Project post](http://a11yproject.com/posts/never-remove-css-outlines/) for more details.
---
+++ js/app.js
@@ -0,0 +1,291 @@
+(function (root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+      define([], factory);
+  } else if (typeof exports === 'object') {
+      module.exports = factory();
+  } else {
+      root.VideoModal = factory();
+  }
+}(this, function () {
+
+  var VideoModal = function (element) {
+      if (!this || !(this instanceof VideoModal)) {
+          return new VideoModal(element);
+      }
+
+      this.selector = document.querySelectorAll(element);
+      this.root     = document.querySelector('body');
+      this.run();
+  };
+
+  VideoModal.prototype = {
+      run: function () {
+          Array.prototype.forEach.call(this.selector, function (el) {
+              el.addEventListener('click', function (e) {
+                  e.preventDefault();
+
+                  var link = this.parseUrl(el.getAttribute('href'));
+                  this.render(link);
+                  this.close();
+              }.bind(this), false);
+          }.bind(this));
+      },
+      template: function (s, d) {
+          var p;
+
+          for (p in d) {
+              if (d.hasOwnProperty(p)) {
+                  s = s.replace(new RegExp('{' + p + '}', 'g'), d[p]);
+              }
+          }
+          return s;
+      },
+      parseUrl: function (url) {
+          var service = {},
+              matches;
+
+          if (matches = url.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/)) {
+              service.provider = "youtube";
+              service.id       = matches[2];
+          } else if (matches = url.match(/https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/)) {
+              service.provider = "vimeo";
+              service.id       = matches[3];
+          } else {
+              service.provider = "Unknown";
+              service.id       = '';
+          }
+
+          return service;
+      },
+      render: function (service) {
+          var embedLink,
+              lightbox;
+
+          if (service.provider === 'youtube') {
+              embedLink = 'https://www.youtube.com/embed/' + service.id;
+          } else if (service.provider === 'vimeo') {
+              embedLink = 'https://player.vimeo.com/video/' + service.id;
+          } else {
+              throw new Error("Invalid video URL");
+          }
+
+          lightbox = this.template(
+              '<div class="videomodal-wrap"><div class="videomodal-content"><span class="videomodal-close"></span><iframe src="{embed}?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div>', {
+                  embed: embedLink
+              });
+
+          this.root.insertAdjacentHTML('beforeend', lightbox);
+      },
+      close: function () {
+          var wrapper = document.querySelector('.videomodal-wrap');
+
+          wrapper.addEventListener('click', function (e) {
+              if (e.target && e.target.nodeName === 'SPAN' && e.target.className === 'videomodal-close') {
+                  wrapper.classList.add('videomodal-hide');
+                  setTimeout(function() {
+                      this.root.removeChild(wrapper);
+                  }.bind(this), 500);
+              }
+          }.bind(this), false);
+      }
+  };
+
+  return VideoModal;
+}));
+
+document.addEventListener('DOMContentLoaded', function() {
+  VideoModal('.video-modal-trigger');
+});
+
+
+
+(function() {
+
+    // Anchor Scroller
+
+  'use-strict';
+
+  var anchors = document.querySelectorAll('a[href^="#"]');
+
+  anchors.forEach(function(anchor) {
+
+    anchor.addEventListener('click', function() {
+
+      var anchorDestination = anchor.getAttribute('href');
+
+      animateScrollTo(document.querySelector(anchorDestination));
+      
+    }, false);
+  });
+})();
+
+(function() {
+
+  // Mobile Menu Control
+
+  'use strict';
+
+  var menuToggles = document.querySelectorAll('.menu-toggle');
+  var mainHeaders = document.querySelectorAll('.main-header');
+  var noScrolls   = document.querySelectorAll('html, body');
+  var menuItems   = document.querySelectorAll('.main-navigation-list-item');
+
+  for (var i = 0; i < menuToggles.length; i++) {
+    menuToggles[i].addEventListener('click',   function() {
+
+      for (var i = 0; i < menuToggles.length; i++) {
+        menuToggles[i].classList.toggle('-active');
+      }
+
+      for (var i = 0; i < mainHeaders.length; i++) {
+        mainHeaders[i].classList.toggle('-active');
+      }
+
+      for (var i = 0; i < noScrolls.length; i++) {
+        noScrolls[i].classList.toggle('-inactive');
+      }
+
+    }, false);
+  }
+
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click',   function() {
+
+      for (var i = 0; i < mainHeaders.length; i++) {
+        mainHeaders[i].classList.remove('-active');
+      }
+
+      for (var i = 0; i < noScrolls.length; i++) {
+        noScrolls[i].classList.remove('-inactive');
+      }
+
+      for (var i = 0; i < menuToggles.length; i++) {
+        menuToggles[i].classList.remove('-active');
+      }
+
+    }, false);
+  }
+
+})();
+
+(function() {
+
+  // Logo List Resize
+
+  "use strict";
+
+  window.onload = function() {
+
+    var images = document.querySelectorAll(".pc-logo-list img");
+    var logoLists = document.querySelectorAll(".pc-logo-list");
+
+    function adjustImageWidth(image) {
+        var widthBase   = 65;
+        var scaleFactor = 0.525;
+        var imageRatio  = image.naturalWidth / image.naturalHeight;
+
+        image.width = Math.pow(imageRatio, scaleFactor) * widthBase;
+    }
+    
+    function activateLogoList(logoList) {
+        logoList.classList.add("pc-logo-list-active");
+    }
+
+    images.forEach(adjustImageWidth);
+    logoLists.forEach(activateLogoList);
+
+  };
+
+}());
+
+$('.testimonial-slider').slick({
+
+  // Testimonial slider
+
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  infinite: true,
+  cssEase: 'linear',
+  arrows: false
+});
+
+(function() {
+      
+  var settings = {
+    cookieExpiration: 365,
+    cookieName: 'acknowledgeCookies'
+  };
+
+  var privacyNotices = document.getElementsByClassName('privacy-notice');
+  var acknowledgeButtons = document.getElementsByClassName('acknowledge-cookies');
+  
+  function createCookie(name, value, days, domain) {
+  
+    var expires;
+    var domain;
+    var date = new Date();
+  
+    if (days) {
+  
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = '; expires=' + date.toGMTString();
+  
+    } else {
+  
+      expires = '';
+    }
+  
+    if (domain) {
+  
+      var domain = '; domain=' + domain;
+    }
+  
+    document.cookie = name + '=' + value + expires + domain + '; path=/';
+  }
+
+  function readCookie(name) {
+  
+    var name = name + '=';
+    var fields = document.cookie.split(';');
+  
+    for(var i=0; i < fields.length; i++) {
+  
+      var field = fields[i].trim();
+  
+      if (field.indexOf(name) == 0) {
+  
+        return field.substring(name.length, field.length);
+      }
+    }
+    return null;
+  }
+  
+  if (!readCookie(settings.cookieName)) {
+
+    for(var i = 0; i < privacyNotices.length; i++) {
+
+      privacyNotices[i].classList.add("privacy-notice-active");
+    }
+  }
+
+
+  for(var i = 0; i < acknowledgeButtons.length; i++) {
+
+    var acknowledgeButton = acknowledgeButtons[i];
+
+    acknowledgeButton.onclick = function() {
+
+      createCookie(settings.cookieName, 1, settings.cookieExpiration);
+
+      for(var i = 0; i < privacyNotices.length; i++) {
+
+        if (privacyNotices[i].classList.contains("privacy-notice-active")) {
+
+          privacyNotices[i].classList.remove("privacy-notice-active");
+        }
+      }
+    }
+  }
+})();
